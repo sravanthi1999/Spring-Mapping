abe2f45e9e8e12ec277bab66b6d3f766784a529f
Sravanthi <shivannagarisravanthi@gmail.com>
2021-06-02 20:04:29 +0530

    html logs

--- src/main/java/com/carMap/CarAppMappingApplication.java
+++ src/main/java/com/carMap/CarAppMappingApplication.java
@@ -2,6 +2,7 @@ package com.carMap;
 
 import org.apache.log4j.Appender;
 import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.HTMLLayout;
 import org.apache.log4j.Layout;
 import org.apache.log4j.Logger;
 import org.apache.log4j.SimpleLayout;
@@ -17,12 +18,23 @@ public class CarAppMappingApplication
 	{
 		//create layout
 		Layout layout=new SimpleLayout();
+		Layout layout2=new HTMLLayout();
+		
 		//create appender+ link layout
 		Appender appender=new ConsoleAppender(layout);
+		Appender appender2=new ConsoleAppender(layout2);
 		
 		SpringApplication.run(CarAppMappingApplication.class, args);
+		
 		//link appender with logger
 		logger.addAppender(appender);
+		logger.addAppender(appender2);
+		
+		logger.info("main class info");
+		logger.debug("main class debug");
+		logger.fatal("main class fatal");
+		logger.error("main class error");
+		logger.warn("main class info");
 		
 	}
 
